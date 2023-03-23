ccc5f20d0f5b616c49d2987809fd2bb7934fe1c4
Amine DJENNADI <amine.djennadi@ocapiat.fr>
2023-03-23 17:50:24 +0100

    init docker

--- back/src/main/java/com/app/ninks-api/NinksApiApplication.java
+++ back/src/main/java/com/app/ninks-api/NinksApiApplication.java
@@ -1,13 +1,13 @@
-package com.app.dashboardapi;
+package com.app.ninksapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DashboardApiApplication {
+public class NinksApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DashboardApiApplication.class, args);
+		SpringApplication.run(NinksApiApplication.class, args);
 	}
 
 }
