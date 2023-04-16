0546158fcbd908145ab698dfd1093b0b9f46b031
amine <amine.djennadi@hotmail.fr>
2023-04-16 12:10:04 +0200

    ajust colors

--- front/src/components/items/ButtonAddLinkComponent.tsx
+++ front/src/components/items/ButtonAddLinkComponent.tsx
@@ -25,14 +25,14 @@ const ButtonAddLinkComponent = (props: Props): JSX.Element => {
             props.onAddItem();
           }}
         />
-        <div className="p-4 rounded-xl border-gray-400 border-2 bg-background-invert-25 place-content-center flex-wrap flex">
-          <span className="rounded-full border-2 border-gray-700 p-0.5">
-            <BiLink size={20} />
+        <div className="p-4 rounded-xl  bg-background-invert-25 place-content-center flex-wrap flex">
+          <span className="rounded-full p-0.5">
+            <BiLink size={20} color="var(--stroke)" />
           </span>
         </div>
         <div className="flex flex-col gap-2">
           <h2 className="font-bold text-xl">Ajouter un lien</h2>
-          <p className="text-paragraph-25 font-bold text-sm opacity-70">Ajouter un super lien pour etre cool</p>
+          <p className="text-gray-500 font-bold text-sm opacity-70">Ajouter un super lien pour etre cool</p>
         </div>
       </div>
     </>
